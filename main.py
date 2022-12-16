2559e9ea126481f6f7f338f89b1801ccf56ef0d9
Arshiya Khalili <106923820+Arshiya-python-developer@users.noreply.github.com>
2022-12-16 23:47:03 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,51 @@
 class PRODUCT:
 
     # Initialized
-    def __init__(self,tittle,short_description,description,slug,permalink,IsAvailable,sku,price,regular_price,sale_price,manage_stock,stock_quantity,IsVisible,date_created_gmt,date_modified_gmt):
+    def __init__(self,tittle,short_description,description,slug,permalink,is_available,sku,price,regular_price,sale_price,manage_stock,stock_quantity,is_visible,date_created_gmt,date_modified_gmt):
         self.tittle = tittle
         self.short_description = short_description
         self.description = description
         self.slug = slug
         self.permalink = permalink
-        self.IsAvailable = IsAvailable
+        self.IsAvailable = is_available
         self.sku = sku
         self.price = price
         self.regular_price = regular_price
         self.sale_price = sale_price
         self.manage_stock = manage_stock
         self.stock_quantity = stock_quantity
-        self.IsVisible = IsVisible
+        self.IsVisible = is_visible
         self.date_created_gmt = date_created_gmt
         self.date_modified_gmt =  date_modified_gmt
 
 
-    def Create(self):
+    def add_product(self):
+        self.tittle = input('please enter tittle of product: ')
+        self.short_description = input('please write short description:')
+        self.description = input('please write more description: ')
+        self.slug = int
+
+
+
+    def read_product(self):
         pass
 
 
-    def Read(self):
+    def update_product(self):
         pass
 
 
-    def Update(self):
+    def delete_product(self):
         pass
 
-
-    def Delete(self):
-        pass
-
-
-items = []
+"""items = []
 while True:
     #display = input('Press enter to continue.')
-    print('1. View items\n2. Add items \n3. Delete items \n4. Edit items\n5. Exit')
+    print('1. Add items\n 1. View items\n3. Delete items \n4. Edit items\n5. Exit')
     choice = input('Enter the number of your choice : ')
+    if choice == '1':
+        Create()
+"""
+
 
 
