dda97e10f7ddfc61613ce94933fb943cd24b7e3a
Arshiya Khalili <106923820+Arshiya-python-developer@users.noreply.github.com>
2022-12-18 17:35:06 +0330

    Update product.py

--- product.py
+++ product.py
@@ -1,7 +1,14 @@
 class PRODUCT:
 
+    _product = []
     # Initialized
-    def __init__(self,id,tittle,short_description,description,slug,permalink,is_available,sku,price,regular_price,sale_price,manage_stock,stock_quantity,is_visible,date_created_gmt,date_modified_gmt):
+    def __init__(self,id:int,tittle:str,short_description:str,description:str,slug:int,permalink:str,is_available:bool,sku,price:float,regular_price:float,sale_price:float,manage_stock:int,stock_quantity:int,is_visible:bool,date_created_gmt,date_modified_gmt):
+
+        assert price >= 0
+        assert regular_price >= 0
+        assert sale_price >= 0
+        assert stock_quantity >= 0
+
         self.id = id
         self.tittle = tittle
         self.short_description = short_description
@@ -19,19 +26,26 @@ class PRODUCT:
         self.date_created_gmt = date_created_gmt
         self.date_modified_gmt =  date_modified_gmt
 
+          
+
+
+    def __repr__(self):
+        return f'Product({self.id},{self.tittle},{self.short_description},{self.description},{self.slug}' \
+               f'{self.sku},{self.permalink},{self.is_visible},{self.price},{self.regular_price},{self.sale_price}' \
+               f'{self.manage_stock},{self.stock_quantity},{self.is_visible},{self.date_created_gmt},{self.date_modified_gmt}'
+
+
 
     def add_product(self):
-        self.tittle = input('please enter tittle of product: ')
-        self.short_description = input('please write short description:')
-        self.description = input('please write more description: ')
-        self.slug = int('please enter how many slug:')
-        self.permalink = input('write your permanent link:')
+
+        return self._product.append([self.id,self.tittle,self.short_description,self.description,self.slug,
+                                     self.permalink,self.is_available,self.sku,self.price,self.regular_price,
+                                     self.sale_price,self.manage_stock,self.stock_quantity])
 
 
 
     def read_product(self):
         pass
-
 
     def update_product(self):
         pass
@@ -40,14 +54,12 @@ class PRODUCT:
     def delete_product(self):
         pass
 
-"""items = []
-while True:
-    #display = input('Press enter to continue.')
-    print('1. Add items\n 1. View items\n3. Delete items \n4. Edit items\n5. Exit')
-    choice = input('Enter the number of your choice : ')
-    if choice == '1':
-        Create()
-"""
+
+
+
+
+
+
 
 
 
