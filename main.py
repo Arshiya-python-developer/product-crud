5f4a959d7e5b797cd7d4566beafb7ac4406714c3
Arshiya Khalili <106923820+Arshiya-python-developer@users.noreply.github.com>
2022-12-21 14:39:20 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ from product import PRODUCT
 Item = PRODUCT(1,'laptop','grea laptop','new model',2,'hhhhh',True,'5',55,56,75,5,5,True,'2022/12/01','2022/12/5')
 
 
+print(Item.add_product())
+print(Item.read_product())
+print(Item.delete_product())
+print(Item.update_product())
 x = isinstance(Item,PRODUCT)
 print(x)
