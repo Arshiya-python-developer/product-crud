cc677bd85e63889d133c6ad0d74b0c886d46bb31
Arshiya Khalili <106923820+Arshiya-python-developer@users.noreply.github.com>
2022-12-21 09:47:01 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ from product import PRODUCT
 Item = PRODUCT(1,'laptop','grea laptop','new model',2,'hhhhh',True,'5',55,56,75,5,5,True,'2022/12/01','2022/12/5')
 
 
-print(Item)
+x = isinstance(Item,PRODUCT)
+print(x)
